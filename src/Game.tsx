dc52208430cd2e343eb4cba703d30a84be4eaf7f
webstar-code <bhaveshrc2001@gmail.com>
2025-01-07 19:15:49 +0530

    first commit

---
+++ src/Game.tsx
@@ -0,0 +1,9 @@
+export function Game() {
+  return (
+    <iframe
+      src="/game.html"
+      title="Squid Game"
+      style={{ width: '100%', height: '100%', border: 'none', marginTop: -60 ,backgroundColor: '#f0f0f0'}}
+    />
+  )
+}
